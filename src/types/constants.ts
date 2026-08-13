6e7fa5287acc6ef297c3f637cccd002b5a923b95
ruying-suixing <Haoxuanzhang2014@outlook.com>
2026-08-13 20:14:42 +0800

    🥰 开源我的博客

---
+++ src/types/constants.ts
@@ -0,0 +1,21 @@
+export const socialLinks = [
+  'github',
+  'gitlab',
+  'gitee',
+  'discord',
+  'youtube',
+  'instagram',
+  'x',
+  'telegram',
+  'rss',
+  'email',
+  'reddit',
+  'bluesky',
+  'tiktok',
+  'weibo',
+  'steam',
+  'bilibili',
+  'zhihu',
+  'coolapk',
+  'netease'
+] as const
